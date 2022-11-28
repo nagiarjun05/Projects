7f40eb753bc3ec28115dd1a4338ab2b47cb47db6
nagiarjun05 <nagiarjun05@gmail.com>
2022-11-29 00:42:13 +0530

    Auto Text Writer

---
+++ Auto_Text_Writer/script.js
@@ -0,0 +1,13 @@
+const text='I will become a Software Developer so soon!!';
+
+let index=0;
+
+function writeText(){
+    document.body.innerText=text.slice(0,index);
+    index++;
+    if(index>text.length){
+        index=0
+    }
+};
+
+setInterval(writeText,50)
