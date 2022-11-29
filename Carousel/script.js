2e266f66117360cccda16aa3a0057492f588979c
nagiarjun05 <nagiarjun05@gmail.com>
2022-11-29 12:24:44 +0530

    Few Adjustments in Image Casoules

--- Carousel/script.js
+++ Carousel/script.js
@@ -6,7 +6,7 @@ let index=0;
 
 function run(){
     index++
-    if (index>img.length){
+    if (index>img.length-1){
         index=0;
     }
     imgs.style.transform=`translateX(${-index*500}px)`;
